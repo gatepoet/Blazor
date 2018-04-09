316633fe495c18f6584fd9ed903ed6fa5ac15470
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-04-09 14:54:49 +0100

    Implementation cleanup

--- src/Microsoft.AspNetCore.Blazor.Browser.JS/src/Rendering/EventDelegator.ts
+++ src/Microsoft.AspNetCore.Blazor.Browser.JS/src/Rendering/EventDelegator.ts
@@ -4,43 +4,56 @@ export interface OnEventCallback {
   (event: Event, componentId: number, eventHandlerId: number, eventArgs: EventForDotNet<UIEventArgs>): void;
 }
 
+// Responsible for adding/removing the eventInfo on an expando property on DOM elements, and
+// calling an EventInfoStore that deals with registering/unregistering the underlying delegated
+// event listeners as required (and also maps actual events back to the given callback).
 export class EventDelegator {
   private static nextEventDelegatorId = 0;
   private eventsCollectionKey: string;
-  private boundGlobalEventHandlerInstance: EventListener = evt => this.onGlobalEvent(evt);
-
-  private activeEventHandlersById: { [eventHandlerId: number]: EventHandlerInfo } = {};
-
-  // Also index first by event name, so we can track whether any handlers remain for a given event name
-  private activeEventHandlersByEventName: { [eventName: string]: { [eventHandlerId: number]: EventHandlerInfo } } = {};
+  private eventInfoStore: EventInfoStore;
 
   constructor(private onEvent: OnEventCallback) {
     const eventDelegatorId = ++EventDelegator.nextEventDelegatorId;
     this.eventsCollectionKey = `_blazorEvents_${eventDelegatorId}`;
+    this.eventInfoStore = new EventInfoStore(this.onGlobalEvent.bind(this));
   }
 
   public setListener(element: Element, eventName: string, componentId: number, eventHandlerId: number) {
-    this.ensureHasGlobalListener(eventName);
-    this.setEventHandlerInfo(element, eventName, componentId, eventHandlerId);
+    // Ensure we have a place to store event info for this element
+    let infoForElement: EventHandlerInfosForElement = element[this.eventsCollectionKey];
+    if (!infoForElement) {
+      infoForElement = element[this.eventsCollectionKey] = {};
+    }
+
+    if (infoForElement.hasOwnProperty(eventName)) {
+      // We can cheaply update the info on the existing object and don't need any other housekeeping
+      const oldInfo = infoForElement[eventName];
+      this.eventInfoStore.update(oldInfo.eventHandlerId, eventHandlerId);
+    } else {
+      // Go through the whole flow which might involve registering a new global handler
+      const newInfo = { element, eventName, componentId, eventHandlerId };
+      this.eventInfoStore.add(newInfo);
+      infoForElement[eventName] = newInfo;
+    }
   }
 
   public removeListener(eventHandlerId: number) {
-    const info = this.activeEventHandlersById[eventHandlerId];
+    // This method gets called whenever the .NET-side code reports that a certain event handler
+    // has been disposed. However we will already have disposed the info about that handler if
+    // the eventHandlerId for the (element,eventName) pair was replaced during diff application.
+    const info = this.eventInfoStore.remove(eventHandlerId);
     if (info) {
-      this.removeEventHandler(info);
+      // Looks like this event handler wasn't already disposed
+      // Remove the associated data from the DOM element
+      const element = info.element;
+      if (element.hasOwnProperty(this.eventsCollectionKey)) {
+        const elementEventInfos: EventHandlerInfosForElement = element[this.eventsCollectionKey];
+        delete elementEventInfos[info.eventName];
+        if (Object.getOwnPropertyNames(elementEventInfos).length === 0) {
+          delete element[this.eventsCollectionKey];
+        }
+      }
     }
-  }
-
-  private ensureHasGlobalListener(eventName: string) {
-    if (!this.activeEventHandlersByEventName.hasOwnProperty(eventName)) {
-      this.activeEventHandlersByEventName[eventName] = {};
-      document.addEventListener(eventName, this.boundGlobalEventHandlerInstance);
-    }
-  }
-
-  private removeGlobalListener(eventName: string) {
-    delete this.activeEventHandlersByEventName[eventName];
-    document.removeEventListener(eventName, this.boundGlobalEventHandlerInstance);
   }
 
   private onGlobalEvent(evt: Event) {
@@ -68,66 +81,60 @@ export class EventDelegator {
       candidateElement = candidateElement.parentElement;
     }
   }
+}
 
-  private setEventHandlerInfo(element: Element, eventName: string, componentId: number, eventHandlerId: number) {
-    const handlersForEventName = this.activeEventHandlersByEventName[eventName];
+// Responsible for adding and removing the global listener when the number of listeners
+// for a given event name changes between zero and nonzero
+class EventInfoStore {
+  private infosByEventHandlerId: { [eventHandlerId: number]: EventHandlerInfo } = {};
+  private countByEventName: { [eventName: string]: number } = {};
 
-    // Ensure we're tracking event info about this element
-    const infoForElement = this.getOrCreateEventHandlerInfos(element);
-
-    // As a very minor optimization, update the existing EventHandlerInfo instance if there is one
-    if (infoForElement.hasOwnProperty(eventName)) {
-      // We can remove the old event handler ID from the index, but there's no need to go through
-      // any bigger disposal process because we're about to add a new handler in its place
-      const handlerInfo = infoForElement[eventName];
-      const oldEventHandlerId = handlerInfo.eventHandlerId;
-      delete handlersForEventName[oldEventHandlerId];
-      delete this.activeEventHandlersById[oldEventHandlerId];
-
-      // The only value that can change is eventHandlerId, so just update that
-      handlerInfo.eventHandlerId = eventHandlerId;
-      handlersForEventName[eventHandlerId] = handlerInfo;
-      this.activeEventHandlersById[eventHandlerId] = handlerInfo;
-    } else {
-      const handlerInfo = { element, eventName, componentId, eventHandlerId };
-      infoForElement[eventName] = handlerInfo;
-      handlersForEventName[eventHandlerId] = handlerInfo;
-      this.activeEventHandlersById[eventHandlerId] = handlerInfo;
-    }
+  constructor(private globalListener: EventListener) {
   }
 
-  private removeEventHandler(info: EventHandlerInfo) {
-    // Remove it from the by-event-ID index
-    const eventHandlerId = info.eventHandlerId;
-    delete this.activeEventHandlersById[eventHandlerId];
+  public add(info: EventHandlerInfo) {
+    if (this.infosByEventHandlerId[info.eventHandlerId]) {
+      // Should never happen, but we want to know if it does
+      throw new Error(`Event ${info.eventHandlerId} is already tracked`);
+    }
 
-    // Remove it from the by-event-name index, and possibly remove the global listener if it was the last one
+    this.infosByEventHandlerId[info.eventHandlerId] = info;
+
     const eventName = info.eventName;
-    const handlersForEventName = this.activeEventHandlersByEventName[eventName];
-    if (handlersForEventName) {
-      delete handlersForEventName[eventHandlerId];
-      if (Object.getOwnPropertyNames(handlersForEventName).length === 0) {
-        this.removeGlobalListener(eventName);
-      }
-    }
-
-    // Remove the associated data from the DOM element
-    const element = info.element;
-    if (element.hasOwnProperty(this.eventsCollectionKey)) {
-      const elementEventInfos: EventHandlerInfosForElement = element[this.eventsCollectionKey];
-      delete elementEventInfos[eventName];
-      if (Object.getOwnPropertyNames(elementEventInfos).length === 0) {
-        delete element[this.eventsCollectionKey];
-      }
+    if (this.countByEventName.hasOwnProperty(eventName)) {
+      this.countByEventName[eventName]++;
+    } else {
+      this.countByEventName[eventName] = 1;
+      document.addEventListener(eventName, this.globalListener);
     }
   }
 
-  private getOrCreateEventHandlerInfos(element: Element) {
-    let result: EventHandlerInfosForElement = element[this.eventsCollectionKey];
-    if (!result) {
-      result = element[this.eventsCollectionKey] = {};
+  public update(oldEventHandlerId: number, newEventHandlerId: number) {
+    if (this.infosByEventHandlerId.hasOwnProperty(newEventHandlerId)) {
+      // Should never happen, but we want to know if it does
+      throw new Error(`Event ${newEventHandlerId} is already tracked`);
     }
-    return result;
+
+    // Since we're just updating the event handler ID, there's no need to update the global counts
+    const info = this.infosByEventHandlerId[oldEventHandlerId];
+    delete this.infosByEventHandlerId[oldEventHandlerId];
+    info.eventHandlerId = newEventHandlerId;
+    this.infosByEventHandlerId[newEventHandlerId] = info;
+  }
+
+  public remove(eventHandlerId: number): EventHandlerInfo {
+    const info = this.infosByEventHandlerId[eventHandlerId];
+    if (info) {
+      delete this.infosByEventHandlerId[eventHandlerId];
+
+      const eventName = info.eventName;
+      if (--this.countByEventName[eventName] === 0) {
+        delete this.countByEventName[eventName];
+        document.removeEventListener(eventName, this.globalListener);
+      }
+    }
+
+    return info;
   }
 }
 
